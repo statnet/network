7e7dc850e61730d35cb19eb5550d2bc1ce3d3861
Skye Bender-deMoll <skyebend@uw.edu>
2013-08-14 17:47:48 +0000

    fixed #56 (Trac: 491) bug in how is.color() processed NA values that mangled colors in plots in some situations.  Added test.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/network/trunk/network@12126 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/plot_tests.R
+++ tests/plot_tests.R
@@ -14,3 +14,16 @@ plot(ynet,edge.label='weight',edge.label.col='blue',edge.label.cex='weight')
 data(emon)
 par(mar=c(0,0,0,0))
 plot(emon[[5]],edge.label=TRUE,edge.label.cex=0.6,edge.col='gray',edge.lwd=(emon[[5]]%e%'Frequency')*2)
+
+col.list<-c('red','#800000','#80000505',NA)
+# test is.color for vector NA processing bug #491
+if(!all(is.color(col.list)[1:3] & is.na(is.color(col.list)[4]))){
+  stop('is.color did not correctly recognize colors and NA values in a character vector')
+}
+   
+col.list<-list('red','#800000','#80000505',NA)
+# test is.color for list NA processing bug #491
+if(!all(is.color(col.list)[1:3] & is.na(is.color(col.list)[4]))){
+  stop('is.color did not correctly recognize colors and NA values in a list')
+}
+
