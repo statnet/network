646513f7c9b10567416e3fb0fa80af2e5324a693
Skye Bender-deMoll <skyebend@uw.edu>
2014-01-09 04:59:29 +0000

    split some tests out of 'network.battery into a file named general.tests to squeeze past the test time restrictions
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/network/trunk/network@12454 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/network.battery.R
+++ tests/network.battery.R
@@ -148,93 +148,6 @@ temp<-set.edge.value(temp,"value",g*matrix(1:100,10,10))
 check[52]<-all(get.edge.value(temp,"value")==(g*matrix(1:100,10,10))[g>0])
 check[53]<-all(as.sociomatrix(temp,"value")==(g*matrix(1:100,10,10)))
 
-# ----- check assigning multiple attribute values in a single call 
-test<-network.initialize(3)
-set.vertex.attribute(test,c('a','b'),c(1,2))
-if(!all(test%v%'a'==c(1,1,1) & test%v%'b'==c(2,2,2))){
-  stop('setting multiple attribute values with set.vertex.attribute failed')
-}
-
-test<-network.initialize(3)
-set.vertex.attribute(test,list('a','b'),c(1,2))
-if(!all(test%v%'a'==c(1,1,1) & test%v%'b'==c(2,2,2))){
-  stop('setting multiple attribute values with set.vertex.attribute failed')
-}
-
-test<-network.initialize(3)
-set.vertex.attribute(test,c('a','b'),list(c(1,2,3),c(4,5,6)))
-if(!all(test%v%'a'==c(1,2,3) & test%v%'b'==c(4,5,6))){
-  stop('setting multiple attribute values with set.vertex.attribute failed')
-}
-
-test<-network.initialize(3)
-set.vertex.attribute(test,c('a','b'),list(list(1,2,3),list(4,5,6)))
-if(!all(test%v%'a'==c(1,2,3) & test%v%'b'==c(4,5,6))){
-  stop('setting multiple attribute values with set.vertex.attribute failed')
-}
-
-test<-network.initialize(3)
-obj<-list(one='a complex object',two=c('with muliple','parts'))
-set.vertex.attribute(test,c('a','b'),list(list(as.list(obj)),list(as.list(obj))))
-if(!all(all.equal(get.vertex.attribute(test,'a',unlist=FALSE)[[1]],obj) & all.equal(get.vertex.attribute(test,'b',unlist=FALSE)[[1]],obj))){
-  stop('setting multiple attribute values with list values in set.vertex.attribute failed')
-}
-
-# check memory saftey with a big assignment
-net<-network.initialize(100000)
-set.vertex.attribute(net,LETTERS,LETTERS)
-
-# test multiple assignment for network
-
-test<-network.initialize(3)
-set.network.attribute(test,c("a","b"),1:2)
-if (!all(test%n%'a'==1,test%n%'b'==2)){
-  stop('mulltiple attribute assignment failed for set.network.attribute')
-}
-
-test<-network.initialize(3)
-set.network.attribute(test,list("a","b"),as.list(1:2))
-if (!all(test%n%'a'==1,test%n%'b'==2)){
-  stop('mulltiple attribute assignment failed for set.network.attribute')
-}
-
-
-
-# test multiple assignment for edges
-
-test<-network.initialize(3)
-add.edges(test,tail=1:3,head=c(2,3,1))
-net<-test
-set.edge.attribute(net,c("a","b"),1:2)
-if (!all(net%n%'a'==1,net%n%'b'==2)){
-  stop('mulltiple attribute assignment failed for set.edge.attribute')
-}
-
-net<-test
-set.edge.attribute(net,c('a','b'),list(c(1,2,3),c(4,5,6)))
-if(!all(net%e%'a'==c(1,2,3) & net%e%'b'==c(4,5,6))){
-  stop('setting multiple attribute values with set.edge.attribute failed')
-}
-
-net<-test
-set.edge.attribute(net,c('a','b'),list(list(1,2,3),list(4,5,6)))
-if(!all(net%e%'a'==c(1,2,3) & net%e%'b'==c(4,5,6))){
-  stop('setting multiple attribute values with set.edge.attribute failed')
-}
-
-net<-test
-obj<-list(one='a complex object',two=c('with muliple','parts'))
-set.edge.attribute(net,c('a','b'),list(list(as.list(obj)),list(as.list(obj))))
-if(!all(all.equal(get.edge.attribute(net,'a',unlist=FALSE)[[1]],obj) & all.equal(get.edge.attribute(net,'b',unlist=FALSE)[[1]],obj))){
-  stop('setting multiple attribute values with list values in set.edge.attribute failed')
-}
-
-# check memory saftey with a big assignment
-net<-network.initialize(100000)
-net<-add.edges(net,1:99999,2:100000)
-set.edge.attribute(net,LETTERS,LETTERS)
-
-
 
 #---- Check additional operators ----
 g<-rgraph(10)
@@ -278,22 +191,14 @@ check[75]<-(network.size(g)==5)&&(temp==8)
 g<-network.initialize(5); (function(){g<-network.initialize(4); add.vertices(g,3)})()
 check[76]<-(network.size(g)==5)
 
-# ---- tests for specific bugs/edgecases -----
-
-# ticket #180 (used to throw error if no edges exist)
-set.edge.attribute(network.initialize(3),"test","a")
-
-# check for network of zero size --used to give error ticket #255
-set.vertex.attribute(network.initialize(0),'foo','bar')
-
 # check for operators with undirected edge error ticket #279
-  # nw1 is assigned tail<head
-  nw1<-network.initialize(3,directed=FALSE)
-  nw1[1,2]<-1
+# nw1 is assigned tail<head
+nw1<-network.initialize(3,directed=FALSE)
+nw1[1,2]<-1
 
-  # nw2 is assigned tail>head
-  nw2<-network.initialize(3,directed=FALSE)
-  nw2[2,1]<-1
+# nw2 is assigned tail>head
+nw2<-network.initialize(3,directed=FALSE)
+nw2[2,1]<-1
 
 # Which, the binary network operators don't take into account:
 check[77]<-network.edgecount(nw1-nw2)==0 # Should have 0, has 1.
@@ -302,41 +207,7 @@ check[79]<-network.edgecount(nw1&nw2)==1 # Should have 1, has 0 (since it treats
 check[80]<-network.edgecount(!nw1)==2 # Should have choose(3,2)-1=2, has 3.
 check[81]<-network.edgecount(!nw2)==2 # Should have choose(3,2)-1=2, has 2.
 
-
-# check get edge attribute overloading
-net<-network.initialize(3)
-add.edges(net,c(1,2,3),c(2,3,1))
-set.edge.attribute(net,'test',"a")
-if(!all(get.edge.attribute(net,'test')==c("a","a","a"))){stop("overloading of get.edge.attribute to get.edge.value not working correctly ")}
-
 #If everything worked, check is TRUE
 if(!all(check)){                                               #Should be TRUE
- stop(paste("network package test failed on test(s):",which(!check)))
+  stop(paste("network package test failed on test(s):",which(!check)))
 }
-
-# check for undirected loops getID cases #327 #609
-net<-network.initialize(2,loops=TRUE,directed=FALSE)
-net[1,1]<-1
-net[1,2]<-1
-net[2,2]<-1
-if(get.edgeIDs(net,v=1,alter=1)!=1){
-  stop("problem with get.edgeIDs on undirected network with loops")
-}
-if(get.edgeIDs(net,v=2,alter=2)!=3){
-  stop("problem with get.edgeIDs on undirected network with loops")
-}
-
-net<-network.initialize(2,loops=TRUE,directed=FALSE)
-net[1,2]<-1
-if(length(get.edgeIDs(net,v=2,alter=2))>0){
-  stop("problem with get.edgeIDs on undirected network with loops")
-}
-
-
-# check for is.na.network problems #619
-x2<-network.initialize(3)
-x2[1,2]<-NA
-if(is.na.network(x2)[1,2]!=1){
-  stop('problem iwth is.na.netowrk')
-}
-
