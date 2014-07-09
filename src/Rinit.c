de1e0466e116234eb4e0641ffefb6751bac10423
Jeffrey Horner <jeffrey.horner@gmail.com>
2014-07-09 23:08:27 +0000

    Add R init C registration routine.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/network/trunk@13163 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ src/Rinit.c
@@ -0,0 +1,67 @@
+/*
+######################################################################
+#
+# utils.c
+#
+# Written by Jeffrey Horner <jeffrey.horner@gmail.com>
+# Last Modified 7/08/2014
+# Licensed under the GNU General Public License version 2 (June, 1991)
+#
+# Part of the R/network package
+#
+# This file contains the R/C initialization code
+#
+######################################################################
+*/
+
+#include <R.h>            
+#include <Rinternals.h>            
+#include <R_ext/Rdynload.h>
+#include "access.h"
+#include "constructors.h"
+#include "layout.h"
+#include "utils.h"
+
+#define CALLDEF(name, n) {#name,(DL_FUNC) &name, n}
+static R_CallMethodDef CallEntries[] = {
+  CALLDEF(addEdge_R,6),
+  CALLDEF(addEdges_R,6),
+  CALLDEF(addVertices_R,3),
+  CALLDEF(copyNetwork_R,1),
+  CALLDEF(deleteEdgeAttribute_R,2),
+  CALLDEF(deleteEdges_R,2),
+  CALLDEF(deleteNetworkAttribute_R,2),
+  CALLDEF(deleteVertexAttribute_R,2),
+  CALLDEF(deleteVertices_R,2),
+  CALLDEF(getEdgeIDs_R,5),
+  CALLDEF(getEdges_R,5),
+  CALLDEF(getNeighborhood_R,4),
+  CALLDEF(isAdjacent_R,4),
+  CALLDEF(isNANetwork_R,2),
+  CALLDEF(networkEdgecount_R,2),
+  CALLDEF(permuteVertexIDs_R,2),
+  CALLDEF(setEdgeAttribute_R,4),
+  CALLDEF(setEdgeAttributes_R,4),
+  CALLDEF(setEdgeValue_R,4),
+  CALLDEF(setNetworkAttribute_R,3),
+  CALLDEF(setVertexAttribute_R,4),
+  CALLDEF(setVertexAttributes_R,4),
+  {NULL,NULL,0}
+};
+
+static R_CMethodDef CEntries[] = {
+  CALLDEF(network_layout_fruchtermanreingold_R,15),
+  CALLDEF(network_layout_kamadakawai_R,10),
+  {NULL,NULL,0}
+};
+
+void R_init_network(DllInfo *dll)
+{
+   R_registerRoutines(dll,CEntries,CallEntries, NULL, NULL);
+   R_useDynamicSymbols(dll, FALSE);
+
+   /* Callable functions from other packages' C code */
+#define RREGDEF(name) R_RegisterCCallable("network", #name, (DL_FUNC) name)
+   RREGDEF(setListElement);
+   RREGDEF(getListElement);
+}
