65556012e4743fc882b108eeada9124cb32296d1
Skye Bender-deMoll <skyebend@uw.edu>
2014-09-24 00:01:37 +0000

    split general.tests file into two files to avoid cran timing warning
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/network/trunk@13338 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/general.tests.R
+++ tests/general.tests.R
@@ -42,10 +42,6 @@ set.network.attribute(netlist[[1]],"test","a value")
 if (!"test" %in% list.network.attributes(netlist[[1]]))
   stop('assignment to list of networks failed')
 
-# check memory saftey with a big assignment
-net<-network.initialize(100000)
-set.vertex.attribute(net,LETTERS,LETTERS)
-
 # test multiple assignment for network
 
 test<-network.initialize(3)
@@ -91,10 +87,6 @@ if(!all(all.equal(get.edge.attribute(net,'a',unlist=FALSE)[[1]],obj) & all.equal
   stop('setting multiple attribute values with list values in set.edge.attribute failed')
 }
 
-# check memory saftey with a big assignment
-net<-network.initialize(100000)
-net<-add.edges(net,1:99999,2:100000)
-set.edge.attribute(net,LETTERS,LETTERS)
 
 
 # ---- checks for  get.edge.attribute overloading and omit args ----
@@ -196,61 +188,3 @@ if(!identical(NULL,get.edge.attribute(net,'nullval',null.na=TRUE))){
   stop("get.edge.attribute not returning NULL values stored as edge attribute correctly")
 }
 
-# ---- check for undirected loops getID cases #327 #609 -----
-net<-network.initialize(2,loops=TRUE,directed=FALSE)
-net[1,1]<-1
-net[1,2]<-1
-net[2,2]<-1
-if(get.edgeIDs(net,v=1,alter=1)!=1){
-  stop("problem with get.edgeIDs on undirected network with loops")
-}
-if(get.edgeIDs(net,v=2,alter=2)!=3){
-  stop("problem with get.edgeIDs on undirected network with loops")
-}
-
-net<-network.initialize(2,loops=TRUE,directed=FALSE)
-net[1,2]<-1
-if(length(get.edgeIDs(net,v=2,alter=2))>0){
-  stop("problem with get.edgeIDs on undirected network with loops")
-}
-
-# --- tests for get.induced.subgraph additions --
-data(emon)
-# extract the network of responders in MtStHelens network with interaction Frequency of 4
-subG4<-get.inducedSubgraph(emon$MtStHelens,eid=which(emon$MtStHelens%e%'Frequency'==4))
-if(network.size(subG4)!=24){
-  stop('wrong size eid induced subgraph')
-}
-
-if (any(subG4%e%'Frequency'!=4)){
-  stop('bad edges in eid induced subgraph')
-}
-
-# checks for error conditions
-# can't specify eid with v or alter
-# get.inducedSubgraph(v=1:2,emon$MtStHelens,eid=which(emon$MtStHelens%e%'Frequency'==4))
-# get.inducedSubgraph(alter=1:2,emon$MtStHelens,eid=which(emon$MtStHelens%e%'Frequency'==4))
-# get.inducedSubgraph(emon$MtStHelens,eid=200:300)
-
-
-# ---- tests for specific bugs/edgecases -----
-
-# ticket #180 (used to throw error if no edges exist)
-set.edge.attribute(network.initialize(3),"test","a")
-
-# check for network of zero size --used to give error ticket #255
-set.vertex.attribute(network.initialize(0),'foo','bar')
-
-
-# check for is.na.network problems #619
-x2<-network.initialize(3)
-x2[1,2]<-NA
-if(is.na.network(x2)[1,2]!=1){
-  stop('problem iwth is.na.netowrk')
-}
-
-# check for na problems in which.matrix.type #926
-mat <- matrix(rbinom(200, 1, 0.2), nrow = 20)
-naIndices <- sample(1:200, 20)
-mat[naIndices] <- NA
-nw <- network(mat)
