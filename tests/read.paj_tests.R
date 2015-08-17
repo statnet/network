175484574d7c72a46879b61f20140ab32b710e31
Skye Bender-deMoll <skyebend@uw.edu>
2015-08-17 23:46:55 +0000

    first stage of significant refactoring of read.paj code, started adding tests
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/network/trunk/network@14008 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ tests/read.paj_tests.R
@@ -0,0 +1,215 @@
+# test for reading pajek formatted files
+require(testthat)
+require(network)
+
+
+# test for case of verticse, but no edges/arcs
+tmptest<-tempfile()
+cat("*Vertices          2
+        1    1231062
+        2    1231095
+*Arcs
+*Edges
+",file=tmptest)
+noEdges<-read.paj(tmptest)
+expect_equal(network.size(noEdges),2)
+expect_equal(network.edgecount(noEdges),0)
+
+
+# check arcs vs edges parsing
+
+# arcs only
+
+tmptest<-tempfile()
+cat("*Vertices          3
+1   'A' 
+2   'B'
+3   'C' 
+*Arcs
+1 2 1
+1 3 1
+",file=tmptest)
+arcsOnly<-read.paj(tmptest)
+expect_true(is.directed(arcsOnly))
+expect_equal(network.edgecount(arcsOnly),2)
+
+# edges only
+tmptest<-tempfile()
+cat('*Vertices      9
+1 "1"    0.3034    0.7561
+2 "2"    0.4565    0.6039
+3 "3"    0.4887    0.8188
+4 "4"    0.5687    0.4184
+5 "5"    0.3574    0.4180
+6 "6"    0.7347    0.2678
+7 "7"    0.9589    0.3105
+8 "8"    0.8833    0.1269
+9 "9"    0.7034    0.0411
+*Arcs
+*Edges
+1      2       1
+1      3       1
+2      3       1
+2      4       1
+2      5       1
+4      5       1
+4      6       1
+6      7       1
+6      8       1
+6      9       1
+7      8       1
+8      9       1 
+',file=tmptest)
+edgesOnly<-read.paj(tmptest)
+expect_false(is.directed(edgesOnly))
+expect_equal(network.edgecount(edgesOnly),12)
+
+
+# both arcs and edges
+# network will be directed, each *edges record will create one arc in each direction
+tmptest<-tempfile()
+cat("*Vertices          4
+1   'A' 
+2   'B' 
+3   'C'
+4   'D'
+*Arcs
+1 2 1
+1 3 1
+*Edges
+3 4 1
+",file=tmptest)
+arcsNEdges<-read.paj(tmptest)
+expect_true(is.directed(arcsNEdges))
+expect_equal(network.edgecount(arcsNEdges),4)
+as.matrix(arcsNEdges)
+
+
+# ----- error testing
+tmptest<-tempfile()
+cat("*Vertices          2
+1   'A' 
+2   'B' 
+*Arcs
+1 
+",file=tmptest)
+expect_error(read.paj(tmptest),regexp = 'does not appear to have the required')
+
+tmptest<-tempfile()
+cat("*Vertices          2
+1   'A' 
+2   'B' 
+*Arcs
+1 A 1
+",file=tmptest)
+expect_error(read.paj(tmptest),regexp = 'contains non-numeric or NA values')
+
+tmptest<-tempfile()
+cat("*Vertices          2
+1   'A' 
+2   'B' 
+*Arcs
+1 2.5 1
+",file=tmptest)
+expect_error(read.paj(tmptest),regexp = 'contains non-integer values')
+
+
+# test stuff in file comments
+########## but multirelational ############ only ~200  nodes 
+#GulfLDays.net 
+#GulfLMonths.net
+#GulfLDow.net 
+#gulfAllDays.net     #GulfADays.zip
+#gulfAllMonths.net   #GulfAMonths.zip
+#LevantDays.net 
+#LevantMonths.net
+#BalkanDays.net 
+#BalkanMonths.net 
+
+#arcs and edges both present   search for " #these have both arc and edge lines " or "URL has a net file"
+#Graph drawing competition page (GD)
+#C95,C95,B98,A99,C99,A99m
+
+
+#things to do:
+#handle ragged array .net files like "CSphd.net"     DONE!!
+#handel two mode networks                            DONE!!
+#handle mix of edges and arcs                        DONE!!
+#handle multirelational pajek files
+
+#issue with read.table and number.cols and fill...SanJuanSur_deathmessage.net has one row with 8 all the rest (including the first 5 have 5)
+
+
+#dschruth added... crashing on Scotland.paj vector length != numOfEdges (http://vlado.fmf.uni-lj.si/pub/networks/data/esna/scotland.htm)
+# appears that this file has charaterset encoding problems
+
+#scotland<-tempfile('scotland',fileext='.zip')
+#download.file('http://vlado.fmf.uni-lj.si/pub/networks/data/esna/scotland.zip',scotland)
+#read.paj(unz(scotland,'Scotland.paj'))
+#tools::showNonASCIIfile(scotland)
+
+
+
+
+
+
+
+
+#read.paj() test links
+#../test/Scotland.paj  ../test/Scotland.net  from http://vlado.fmf.uni-lj.si/pub/networks/data/esna/
+#
+A95net<-read.paj("http://vlado.fmf.uni-lj.si/pub/networks/data/GD/gd95/A95.net")
+expect_equal(network.size(A95net),36)
+expect_equal(network.vertex.names(A95net)[1:5],c("MUX","INSTRUCTION BUFFER (4 x 16)", "RCV","DRV","ROM REG"))
+
+# test reading a .paj project file
+bkfratProj<-read.paj('http://vlado.fmf.uni-lj.si/pub/networks/data/ucinet/bkfrat.paj')
+
+# should have two networks
+expect_equal(sapply(bkfratProj,class),c('network','network'),check.attributes=FALSE)
+# .. with wierd names
+expect_equal(names(bkfratProj),c('UciNet\\BKFRAT.DAT : BKFRAB','UciNet\\BKFRAT.DAT : BKFRAC'))
+# and 58 vertices
+expect_equal(sapply(bkfratProj,network.size),c(58,58),check.attributes=FALSE)
+expect_equal(sapply(bkfratProj,network.edgecount),c(1934,3306),check.attributes=FALSE)
+
+#check edge values and attribute naming
+expect_equal((bkfratProj[[1]]%e%"UciNet\\BKFRAT.DAT : BKFRAB")[1900:1934],c(1, 1, 1, 5, 2, 4, 2, 1, 3, 1, 3, 1, 2, 5, 1, 1, 1, 2, 1, 2, 2, 1, 6, 2, 1, 2, 2, 1, 1, 1, 1, 3, 3, 1, 1))
+
+# check vert attrs
+expect_equal(list.vertex.attributes(bkfratProj[[1]]),c('na','vertex.names','x','y','z'))
+
+# check network attrs
+expect_equal(bkfratProj[[1]]%n%'title',"UciNet\\BKFRAT.DAT : BKFRAB")
+expect_equal(bkfratProj[[2]]%n%'title',"UciNet\\BKFRAT.DAT : BKFRAC")
+
+
+#  ----- examples from http://vlado.fmf.uni-lj.si/pub/networks/doc/ECPR/08/ECPR01.pdf ---
+
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/GraphSet.net
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/TinaSet.net
+
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/GraphList.net  # arcslist
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/TinaList.net  # arcslist
+
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/GraphMat.net  # matrix
+
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/Tina.paj    # partition
+
+# --- crude timing info --
+# not currently supported, but should not fail and should be added as attribute
+timetest<-read.paj('http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/Time.net')
+expect_equal(timetest%e%'pajekTiming',c("[7]","[6-8]"))
+
+
+# http://vlado.fmf.uni-lj.si/pub/networks/pajek/data/Sampson.net  # multiple networks
+# http://vlado.fmf.uni-lj.si/pub/networks/pajek/data/SampsonL.net # multiple networks
+
+# http://vlado.fmf.uni-lj.si/pub/networks/data/2mode/sandi/sandi.net  # two-mode
+# http://vlado.fmf.uni-lj.si/vlado/podstat/AO/net/Davis.paj         # two-mode
+
+#http://vlado.fmf.uni-lj.si/pub/networks/data/GD/gd96/A96.net
+#http://vlado.fmf.uni-lj.si/pub/networks/data/ucinet/bkfrat.paj
+
+
+# temporal versions http://vlado.fmf.uni-lj.si/pub/networks/data/KEDS/KEDS.htm
