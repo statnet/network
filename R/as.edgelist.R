dc14b0f005204fb93a7ea151e360dd608a668c34
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-09-25 17:50:24 +1000

    Removed the obsolete "inverted" edgelist attribute.

--- R/as.edgelist.R
+++ R/as.edgelist.R
@@ -16,23 +16,20 @@ as.edgelist <- function(x, ...){
 
 # convert a network into an ergm-style sorted edgelist using
 # as.edgelist.matrix and as.matrix.network.edgelist
-as.edgelist.network <- function(x, attrname = NULL, as.sna.edgelist = FALSE, inverted = NULL, ...){
+as.edgelist.network <- function(x, attrname = NULL, as.sna.edgelist = FALSE, ...){
   
-  # if inverted is given, use it, otherwise check for network property named inverted, otherwise false
-  inverted<-ifelse(!is.null(inverted),inverted, ifelse(!is.null(x%n%"inverted"),x%n%"inverted", FALSE))
   as.edgelist(as.matrix.network.edgelist(x, attrname=attrname,
                                          as.sna.edgelist=as.sna.edgelist,...), 
                                          n=network.size(x), 
                                          directed=is.directed(x), 
                                          bipartite=ifelse(is.bipartite(x),x%n%"bipartite",FALSE), 
                                          loops=has.loops(x), 
-                                         inverted=inverted,
                                          vnames=network.vertex.names(x))
 }
 
 
 
-as.edgelist.matrix <- function(x, n, directed=TRUE, bipartite=FALSE, loops=FALSE, inverted=FALSE, vnames=seq_len(n),...){
+as.edgelist.matrix <- function(x, n, directed=TRUE, bipartite=FALSE, loops=FALSE, vnames=seq_len(n),...){
   if(!directed) x[,1:2] <- cbind(pmin(x[,1],x[,2]),pmax(x[,1],x[,2]))
   if(!loops) x <- x[x[,1]!=x[,2],,drop=FALSE]
   if(bipartite) x <- x[(x[,1]<=bipartite)!=(x[,2]<=bipartite),,drop=FALSE]
@@ -42,7 +39,6 @@ as.edgelist.matrix <- function(x, n, directed=TRUE, bipartite=FALSE, loops=FALSE
   attr(x,"directed") <- directed
   attr(x,"bipartite") <- bipartite
   attr(x,"loops") <- loops
-  attr(x,"inverted") <- inverted
   class(x)<-c('edgelist',class(x))
   x
 }
