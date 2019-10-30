44d3d70f3397045a0a7957fb09d23a52e450093c
Brendan <brendan.g.knapp@gmail.com>
2019-10-30 08:20:49 -0700

    formatting

--- R/coercion.R
+++ R/coercion.R
@@ -595,7 +595,7 @@ network_from_data_frame <- function(edges, directed = TRUE, vertices = NULL,
     }
   }
   
-  if (any(is.na(edges[,1L:2L]))) {
+  if (any(is.na(edges[, 1L:2L]))) {
     stop("`edges` contains `NA` elements in its first two columns.")
   }
   
