7eff3c560e22a75a67171b662cf585c631e972bd
Michal Bojanowski <michal2992@gmail.com>
2020-10-07 14:59:54 +0200

    Testing undir and bipartite nets
    References statnet/network#39.

--- tests/testthat/test-mixingmatrix.R
+++ tests/testthat/test-mixingmatrix.R
@@ -1,18 +1,27 @@
-# Directed networks
 
+# Naive mixing matrix
 stupid_mm <- function(net, a, ...) {
   edb <- as.data.frame(net, unit="edges")
   vdb <- as.data.frame(net, unit="vertices")
   edb$.head.a <- vdb[[a]][match(edb$.head, vdb$vertex.names)]
   edb$.tail.a <- vdb[[a]][match(edb$.tail, vdb$vertex.names)]
-  # +0 to have numeric not integer
   with(edb, table(From = .tail.a, To = .head.a, ...))
 }
 
-data(emon)
-emm <- stupid_mm(emon$Texas, "Location", exclude=NULL)
+data(emon, package="network")
+data(flo, package="network")
+flonet <- as.network(flo, directed=FALSE)
+set.seed(666)
+flonet %v% "a" <- sample(c(1,2,NA), network.size(flonet), replace=TRUE)
+# plot(flonet, vertex.col = "a")
 
-test_that("mixingmatrix() works on a directed network", {
+# Directed networks -------------------------------------------------------
+
+
+
+
+test_that("mixingmatrix() just works on a directed network", {
+  emm <- stupid_mm(emon$Texas, "Location", exclude=NULL)
   mm <- mixingmatrix(emon$Texas, "Location")
   expect_identical(
     mm$matrix,
@@ -20,6 +29,59 @@ test_that("mixingmatrix() works on a directed network", {
   )
 })
 
-# Undirected networks
+test_that("directed: rows and cols for NA on attribute are always shown", {
+  expect_identical(
+    mixingmatrix(emon$MtSi, "Formalization")$matrix,
+    stupid_mm(emon$MtSi, "Formalization", exclude=NULL)
+  )
+})
 
 
+
+
+
+
+# Undirected networks -----------------------------------------------------
+
+
+test_that("mixingmatrix() just works on a directed network", {
+  net <- network.initialize(4, directed=FALSE)
+  net[1,2] <- net[1,3] <- 1
+  net %v% "a" <- c(1,1, 2,2)
+  expect_identical(
+    mixingmatrix(net, "a")$matrix,
+    structure(
+      matrix(c(1,1,1,0), 2, 2),
+      dimnames = list(From = 1:2, To = 1:2),
+      class = "table"
+    )
+  )
+})
+
+
+test_that("undirected: rows and cols for NA on attribute are always shown", {
+  expect_identical(
+    mixingmatrix(flonet, "a")$matrix,
+    stupid_mm(flonet, "a", exclude=NULL)
+  )
+})
+
+
+# Bipartite networks ------------------------------------------------------
+
+
+test_that("mixingmatrix() just works on a bipartite network", {
+  am <- matrix(0, 5, 5)
+  am[1,3] <- am[2,4] <- 1
+  net <- as.network(am, directed=FALSE, bipartite=3)
+  net %v% "mode" <- c(1,1,2,2,2)
+  net %v% "a" <- c(1,2,1,2,1,2)
+  expect_equivalent(    # TODO should test identical, but mixingmatrix() returns int or num...
+    mixingmatrix(net, "mode")$matrix,
+    structure(
+      matrix(c(2,0), 2, 1),
+      dimnames = list(From = 1:2, To=2),
+      class = "table"
+    )
+  )
+})
